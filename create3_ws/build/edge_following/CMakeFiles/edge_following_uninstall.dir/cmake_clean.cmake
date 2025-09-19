e11fde322b8488a13235194ac518b53cd789b7b2
ShawnHanpro <hanshengyu307@gmail.com>
2025-09-19 10:57:33 +0800

    fix .git preclude create3_ws log install build

--- create3_ws/build/edge_following/CMakeFiles/edge_following_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/edge_following_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/edge_following_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
